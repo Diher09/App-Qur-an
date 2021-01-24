1b85a23fe0fd0138c5b652a2623be8fd35cb88ff
DIHER09 <didithrwn09@gmail.com>
2021-01-24 10:45:27 +0800

    revisi google maps

--- app/src/androidTest/java/com/sisfo/quran/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/sisfo/quran/ExampleInstrumentedTest.java
@@ -18,6 +18,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.azhar.quran", appContext.getPackageName());
+        assertEquals("com.sisfo.quran", appContext.getPackageName());
     }
 }
